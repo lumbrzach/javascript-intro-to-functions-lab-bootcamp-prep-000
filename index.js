3811d49694bdae88b986a81c0c2e5d73efa38e37
Zach Parker <parkerz410@gmail.com>
2019-05-02 16:56:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === lowercase) {
-    return "I can\'t hear you"
+    return 'I can\'t hear you'
   } else if (string === uppercase) {
-    return "YES INDEED!"
+    return 'YES INDEED!'
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
