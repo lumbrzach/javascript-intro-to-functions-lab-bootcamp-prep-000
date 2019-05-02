ff48ba53522c8c694666c4c6454d18db5644627e
Zach Parker <parkerz410@gmail.com>
2019-05-02 16:45:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase());
 };
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+};
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you"
+  }; else if (string === uppercase) {
+    return "YES INDEED!"
+  }; else if (string === "I love you, Grandma.") {
+    return "I love you too."
+  }; else () {
+    return "Error"
+  };
+};
