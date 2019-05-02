8cc30252bc25b7aef7b0fe533fe9fbd9689c5296
Zach Parker <parkerz410@gmail.com>
2019-05-02 17:26:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   var lowercase = string.toLowerCase();
   var uppercase = string.toUpperCase();
   if (string === lowercase) {
-    return "I can\'t hear you"
+    return "I can\'t hear you!"
   } else if (string === uppercase) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
